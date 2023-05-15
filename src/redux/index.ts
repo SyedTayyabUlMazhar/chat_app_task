717be1e40b2a41f4e3d6fc0f34e795853cd48167
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2023-05-15 17:46:15 +0500

    Install redux and related packages and cree store file.

---
+++ src/redux/index.ts
@@ -0,0 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {persistReducer, persistStore} from 'redux-persist';
+
+const reducers = combineReducers({});
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware => {
+    const middlewares = getDefaultMiddleware({
+      thunk: {
+        extraArgument: {
+          thunk: true,
+        },
+      },
+    });
+
+    return middlewares;
+  },
+});
+
+const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+export {store, persistor};
