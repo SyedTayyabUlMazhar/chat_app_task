8f451aab834f9dad8087cff895fdc2a7a9938195
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2023-05-17 12:55:13 +0500

    Navigator: Add themed styling to screens.

--- src/navigator/index.tsx
+++ src/navigator/index.tsx
@@ -2,12 +2,22 @@ import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
 import Screens from '../screens';
 import {ParamList} from './types';
+import useTheme from '../hooks/useTheme';
 
 const Stack = createStackNavigator<ParamList>();
 
 const Navigator = () => {
+  const theme = useTheme();
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        cardStyle: {backgroundColor: theme.background},
+        headerStyle: {
+          backgroundColor: theme.background,
+          shadowColor: 'transparent',
+        },
+        headerTintColor: theme.secondaryText,
+      }}>
       <Stack.Screen
         name="SignUp"
         options={{title: 'Sign Up'}}
