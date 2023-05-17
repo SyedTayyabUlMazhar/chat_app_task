5deb963b091f5e625c7afda990344f6e52a9f1d2
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2023-05-17 19:52:18 +0500

    Create SignUp success action.

--- src/redux/actions/user/reducer.ts
+++ src/redux/actions/user/reducer.ts
@@ -1,5 +1,8 @@
 import {createActionCreator} from '../../ActionCreator';
+import {SignUpSuccessPayload} from './types';
 
 const signInSuccess = createActionCreator('SIGN_IN_SUCCESS');
+const signUpSuccess =
+  createActionCreator<SignUpSuccessPayload>('SIGN_UP_SUCCESS');
 
-export {signInSuccess};
+export {signInSuccess, signUpSuccess};
