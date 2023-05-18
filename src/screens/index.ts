8ffc2c25e9fd70ae10425b3dcd72786cc09a8ecc
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2023-05-18 19:08:56 +0500

    Create a blank home screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,7 @@
 import SignUp from './SignUp';
 import LogIn from './LogIn';
+import Home from './Home';
 
-const Screens = {SignUp, LogIn};
+const Screens = {SignUp, LogIn, Home};
 
 export default Screens;
