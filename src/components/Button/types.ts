e054ca47cf77db7a64a71fcbbbeefbb20d63a495
SyedTayyabUlMazhar <stayyabmazhar19991@gmail.com>
2023-05-18 12:00:12 +0500

    Button: Update type definition for FilledButtonProps

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,13 +1,7 @@
 import {StyleProp} from 'react-native';
 import {TextStyle, TouchableOpacityProps} from 'react-native/types';
 
-export type FilledButtonProps = TouchableOpacityProps & {
-  title: string;
-  fillParent?: boolean;
-  titleStyle?: StyleProp<TextStyle>;
-  disabled?: boolean;
-  isLoading?: boolean;
-};
+export type FilledButtonProps = BaseButtonProps;
 
 export type BaseButtonProps = TouchableOpacityProps & {
   title: string;
