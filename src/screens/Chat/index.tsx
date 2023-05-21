43cc4f1231c0667dceaf98f804d156bdbaced219
SyedTayyabUlMazhar <43899564+SyedTayyabUlMazhar@users.noreply.github.com>
2023-05-22 00:37:40 +0500

    Chat: Use existing room id if passed

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -6,13 +6,18 @@ import Collections from '../../collections';
 import Components from '../../components';
 import useSelector from '../../hooks/useSelector';
 import Routes from '../../navigator/routes';
-import {AppStackScreenProps, ChatRoom, UserChatRoom} from '../../types';
+import {
+  AppStackScreenProps,
+  ChatMessage,
+  ChatRoom,
+  UserChatRoom,
+} from '../../types';
 import ChatUtil from '../../utils/ChatUtil';
 
 export function Chat(props: AppStackScreenProps<typeof Routes.Chat>) {
   const {
     route: {
-      params: {otherUser},
+      params: {otherUser, chatRoomId},
     },
   } = props;
 
@@ -37,28 +42,43 @@ export function Chat(props: AppStackScreenProps<typeof Routes.Chat>) {
   const onSend = useCallback(
     async (newMessages: IMessage[] = []) => {
       console.log('Messages:', newMessages);
-      const room = createChatRoom(newMessages[0]);
-      await Collections.ChatRooms.doc(room.uid).set(room);
+      if (!chatRoomId) {
+        console.log('onSend: ChatRoom doesnt exist will creat new ');
+        const room = createChatRoom(newMessages[0]);
+        await Collections.ChatRooms.doc(room.uid).set(room);
 
-      const chatRoomForSenderUser: UserChatRoom = {
-        roomId: room.uid,
-        otherUserId: otherUser.uid,
-      };
-      const senderUserUpdate = Collections.Users.doc(currentUser.uid).update({
-        chatRooms: firestore.FieldValue.arrayUnion(chatRoomForSenderUser),
-      });
+        const chatRoomForSenderUser: UserChatRoom = {
+          roomId: room.uid,
+          otherUserId: otherUser.uid,
+        };
+        const senderUserUpdate = Collections.Users.doc(currentUser.uid).update({
+          chatRooms: firestore.FieldValue.arrayUnion(chatRoomForSenderUser),
+        });
 
-      const chatRoomForReceiverUser: UserChatRoom = {
-        roomId: room.uid,
-        otherUserId: currentUser.uid,
-      };
-      const receiverUserUpdate = Collections.Users.doc(otherUser.uid).update({
-        chatRooms: firestore.FieldValue.arrayUnion(chatRoomForReceiverUser),
-      });
+        const chatRoomForReceiverUser: UserChatRoom = {
+          roomId: room.uid,
+          otherUserId: currentUser.uid,
+        };
+        const receiverUserUpdate = Collections.Users.doc(otherUser.uid).update({
+          chatRooms: firestore.FieldValue.arrayUnion(chatRoomForReceiverUser),
+        });
 
-      await Promise.all([senderUserUpdate, receiverUserUpdate]);
+        await Promise.all([senderUserUpdate, receiverUserUpdate]);
+      } else {
+        console.log('onSend: ChatRoom exists');
+        const message: ChatMessage = ChatUtil.chatMessageFromIMessage(
+          newMessages[0],
+          otherUser.uid,
+        );
+
+        await Collections.ChatRooms.doc(chatRoomId).update({
+          messages: firestore.FieldValue.arrayUnion(message),
+          lastMessage: message,
+        });
+        console.log('onSend: message added to existing chatRoom: ', chatRoomId);
+      }
     },
-    [createChatRoom, currentUser.uid, otherUser.uid],
+    [chatRoomId, createChatRoom, currentUser.uid, otherUser.uid],
   );
 
   return (
