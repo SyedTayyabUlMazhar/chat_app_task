3dceec4b60f5d5be8f86f0064d72a576a224449d
SyedTayyabUlMazhar <43899564+SyedTayyabUlMazhar@users.noreply.github.com>
2023-05-22 00:13:12 +0500

    Collections: Add ChatRoom collection

--- src/collections/index.ts
+++ src/collections/index.ts
@@ -1,8 +1,9 @@
 import firestore from '@react-native-firebase/firestore';
-import {User} from '../types';
+import {ChatRoom, User} from '../types';
 
 const Collections = {
   Users: firestore().collection<User>('Users'),
+  ChatRooms: firestore().collection<ChatRoom>('ChatRooms'),
 };
 
 export default Collections;
